5697949e4f3a500553399271223ca3eb346e72b8
ericksont11 <ericksont91@gmail.com>
2022-01-07 23:41:30 -0500

    added instructors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 const students = require('./students.json')
 const topics = require('./topics.json')
+const instructors = require('./instructors.json')
 const cors = require('cors')
 
 app.use(cors())
@@ -13,6 +14,10 @@ app.get('/students', (req, res) => {
 
 app.get('/topics', (req, res) => {
   res.send(topics)
+})
+
+app.get('/instructors', (req, res) => {
+  res.send(instructors)
 })
 
 app.listen(port, () => {
